ad9d8c0a25a8df57996aec8b68bac5bb2c872106
chanmmn <33271651+chanmmn@users.noreply.github.com>
2022-03-09 00:57:24 +0800

    Commit FindInvalidEmails for SQL CLR Function
    Commit FindInvalidEmails for SQL CLR Function

---
+++ FindInvalidEmails/FindInvalidEmails/CLRFunction.sql
@@ -0,0 +1,25 @@
+use AdventureWorksLT2019;  
+go  
+  
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'FindInvalidEmails')  
+   DROP FUNCTION FindInvalidEmails;  
+go  
+  
+IF EXISTS (SELECT name FROM sys.assemblies WHERE name = 'MyClrCode')  
+   DROP ASSEMBLY MyClrCode;  
+go  
+  
+CREATE ASSEMBLY MyClrCode FROM 'C:\test\other\FindInvalidEmails\FindInvalidEmails\bin\Debug\FindInvalidEmails.dll'  
+WITH PERMISSION_SET = SAFE -- EXTERNAL_ACCESS;  
+GO  
+  
+CREATE FUNCTION FindInvalidEmails(@ModifiedSince datetime)   
+RETURNS TABLE (  
+   CustomerId int,  
+   EmailAddress nvarchar(4000)  
+)  
+AS EXTERNAL NAME MyClrCode.UserDefinedFunctions.[FindInvalidEmails];  
+go  
+  
+SELECT * FROM FindInvalidEmails('2000-01-01');  
+go  
---
+++ FindInvalidEmails/FindInvalidEmails/clr strict security.sql
@@ -0,0 +1,7 @@
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+
+EXEC sp_configure 'clr strict security', 0;
+RECONFIGURE;
+
+EXEC sp_configure 'clr enabled', 1;  RECONFIGURE WITH OVERRIDE;
