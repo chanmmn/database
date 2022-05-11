dc3a326402407adafbdba5aa85875bab5f65ef0c
chanmmn <33271651+chanmmn@users.noreply.github.com>
2022-05-11 09:00:44 -0700

    Commit ConAppEncrypt TDS
    Commit ConAppEncrypt TDS

---
+++ ConAppEncrypt/ConAppEncrypt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConAppEncrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestTDS();
+        }
+
+        public static void TestTDS()
+        {
+            string connectionString = "Server=172.29.99.222;Database=northwind;User ID=sa;Password=password;MultipleActiveResultSets=True;Encrypt=Yes;TrustServerCertificate=Yes";
+            //string connectionString = "Server=172.29.99.222;Database=northwind;User ID=sa;Password=password;MultipleActiveResultSets=True;";
+            string strSql = "SELECT TOP 10 * FROM Products";
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(strSql, conn);
+            SqlDataReader rd = cmd.ExecuteReader();
+        }
+    }
+}
