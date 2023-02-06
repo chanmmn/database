d89841dc00eaabb0d0f5834009288c8adcd97cc0
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-02-07 00:38:08 +0800

    Commit ConsAppUnitTestDB
    Commit ConsAppUnitTestDB

---
+++ ConsAppUnitTestDB/ConsAppUnitTestDB/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppUnitTestDB
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program program = new Program();
+            //long count = program.CountCMF("11000051");
+            //long count = CountCMF("11000051");
+            long count = CountNWOrder(10248);
+        }
+
+        public static long CountCMF(string customerId)
+        {
+            long count = 0;
+
+            string strconn = @"data source=.;initial catalog=MUFGDBXP;integrated security=True;MultipleActiveResultSets=True;";
+            string strsql = "SELECT COUNT(CustomerCode) FROM MasterCustomerDaily Where CustomerCode = '" + customerId + "'";
+
+            SqlConnection conn = new SqlConnection(strconn);
+            conn.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(strsql, conn);
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "MasterCustomerDaily");
+            //count = ds.Tables[0].Rows.Count;
+            count = long.Parse(ds.Tables[0].Rows[0][0].ToString());
+            return count;
+        }
+
+        public static long CountNWOrder(int orderId)
+        {
+            long count = 0;
+
+            string strconn = @"data source=.;initial catalog=northwind;integrated security=True;MultipleActiveResultSets=True;";
+            string strsql = "SELECT Count([OrderID]) from [dbo].[Order Details] Where OrderId =" + orderId;
+
+            SqlConnection conn = new SqlConnection(strconn);
+            conn.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(strsql, conn);
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "OrderDetails");
+            //count = ds.Tables[0].Rows.Count;
+            count = long.Parse(ds.Tables[0].Rows[0][0].ToString());
+            return count;
+        }
+    }
+}
---
+++ ConsAppUnitTestDB/ConsAppUnitTestDBTests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsAppUnitTestDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppUnitTestDB.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CountCMFTest()
+        {
+            long expected = 9;
+            long actual = Program.CountCMF("11000051");
+    
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
