36017c6e6fdbb3bd674f7507ae284ab7d8df15b5
chanmmn <33271651+chanmmn@users.noreply.github.com>
2024-08-21 00:16:12 +0800

    Commit ConAppWriteXMLSql
    Commit ConAppWriteXMLSql

---
+++ 2024/ConAppWriteXMLSql/ConAppWriteXMLSql/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ConAppWriteXMLSql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlFilePath = "XMLFileRemoveUTF8.xml";
+            string connectionString = "Server=.;Database=poc;Integrated Security=True;TrustServerCertificate=true";
+            int id = 1;
+
+            // Read the XML file
+            string xmlContent = File.ReadAllText(xmlFilePath);
+
+            // Insert the XML into the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO tblMyXml (id, MyXml) VALUES (@id, @xml)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@xml", xmlContent);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("XML inserted into the database successfully.");
+        }
+    }
+}
