dc6d4e1e521c6c457652e9b38f04ac1551395249
chanmmn <33271651+chanmmn@users.noreply.github.com>
2025-07-26 00:28:39 +0800

    Commit datasql

---
+++ 2025/datasql/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        ReadProducts();
+    }
+
+    public static void ReadProducts()
+    {
+        var connectionString = "Server=172.24.222.191;Database=Northwind;User Id=sa;Password=password;TrustServerCertificate=true;";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("SELECT TOP 10 * FROM Products", connection);
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ProductID: {reader["ProductID"]}, ProductName: {reader["ProductName"]}");
+                }
+            }
+        }
+    }
+}
